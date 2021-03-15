4d9252e10de93cb4812eb09e3f0b0a6721d6f948
Teejay <tstech3011@gmail.com>
2021-03-15 12:58:34 +0530

    upto video 3.3

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+// import { Container } from 'react-bootstrap'
+import Container from "react-bootstrap/Container";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+
+function App() {
+	return (
+		<Router>
+			<Header />
+			<main className="py-3">
+				<Container>
+					{/* <h1>Welcome</h1> */}
+					{/* <HomeScreen /> */}
+					<Route path="/" component={HomeScreen} exact />
+					<Route path="/product/:id" component={ProductScreen} />
+				</Container>
+			</main>
+			<Footer />
+		</Router>
+	);
+}
+
+export default App;
