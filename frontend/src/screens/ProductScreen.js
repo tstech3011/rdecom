cc296edd6f4899d012f889bfc44e26eb2c972ae3
Teejay <tstech3011@gmail.com>
2021-03-21 16:09:50 +0530

    Cart Screen Done; Chapter 5)

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,13 +1,23 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import { Row, Col, Image, ListGroup, Button, Card } from "react-bootstrap";
+import {
+	Row,
+	Col,
+	Image,
+	ListGroup,
+	Button,
+	Card,
+	Form,
+} from "react-bootstrap";
 import Rating from "../components/Rating";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
 import { listProductDetails } from "../actions/productActions";
 
-function ProductScreen({ match }) {
+function ProductScreen({ match, history }) {
+	const [qty, setQty] = useState(1);
+
 	const dispatch = useDispatch();
 	const productDetails = useSelector((state) => state.productDetails);
 	const { error, loading, product } = productDetails;
@@ -15,6 +25,10 @@ function ProductScreen({ match }) {
 	useEffect(() => {
 		dispatch(listProductDetails(match.params.id));
 	}, [dispatch, match]);
+
+	const addToCartHandler = () => {
+		history.push(`/cart/${match.params.id}?qty=${qty}`);
+	};
 
 	return (
 		<div>
@@ -65,7 +79,6 @@ function ProductScreen({ match }) {
 										</Col>
 									</Row>
 								</ListGroup.Item>
-
 								<ListGroup.Item>
 									<Row>
 										<Col>Status: </Col>
@@ -76,9 +89,9 @@ function ProductScreen({ match }) {
 										</Col>
 									</Row>
 								</ListGroup.Item>
-
 								<ListGroup.Item>
 									<Button
+										onClick={addToCartHandler}
 										className="btn-block"
 										disabled={product.countInStock === 0}
 										type="button"
@@ -86,6 +99,37 @@ function ProductScreen({ match }) {
 										Add to Cart
 									</Button>
 								</ListGroup.Item>
+								{product.countInStock > 0 && (
+									<ListGroup.Item>
+										<Row>
+											<Col className="my-3 text-center">
+												Qty
+											</Col>
+											<Col xs="auto" className="my-1">
+												<Form.Control
+													as="select"
+													value={qty}
+													onChange={(e) =>
+														setQty(e.target.value)
+													}
+												>
+													{[
+														...Array(
+															product.countInStock
+														).keys(),
+													].map((x) => (
+														<option
+															key={x + 1}
+															value={x + 1}
+														>
+															{x + 1}
+														</option>
+													))}
+												</Form.Control>
+											</Col>
+										</Row>
+									</ListGroup.Item>
+								)}
 							</ListGroup>
 						</Card>
 					</Col>
